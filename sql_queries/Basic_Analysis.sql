6d1ed8a51e9b946bd294a633aa249c8b82442d63
Vinit <135201337+Vinit-data@users.noreply.github.com>
2026-03-10 15:48:40 +0530

    Update Basic_Analysis.sql

--- sql_queries/Basic_Analysis.sql
+++ sql_queries/Basic_Analysis.sql
@@ -13,13 +13,12 @@ from pizzatypess join pizzas
 on pizzatypess.﻿pizza_type_id=pizzas.pizza_type_id
 order by pizzas.price desc limit 1;
 
--- 
+
 -- Identify the most Common pizza size ordered
  select pizzas.size , count(order_details.order_details_id) as order_count
 from pizzas join order_details
 on pizzas.pizza_id=order_details.pizza_id
-group by pizzas.size order by 1
--- group by pizzas.size order by order_count desc
+group by pizzas.size order by order_count desc
 ;
  
 -- List the top 5 most ordered pizza types along with their quantities.
